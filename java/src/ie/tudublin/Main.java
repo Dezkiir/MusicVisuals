326cd3f0440da6c6889712a2100eb1b519225703
Dezkiir <Dezkiir@outlook.com>
2020-03-17 11:15:08 +0000

    At it again

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,6 +2,7 @@ package ie.tudublin;
 
 import C18339746.CubeVisual;
 import C18339746.MyVisual;
+import C18339746.MyVisualTest;
 import C18339746.WaveForm;
 
 public class Main
@@ -10,20 +11,13 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new CubeVisual());
-		
-	}
-
-	public void Wave()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new WaveForm());
+        processing.core.PApplet.runSketch( a, new MyVisualTest());
 		
 	}
 
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.Wave();			
+		main.startUI();			
 	}
 }

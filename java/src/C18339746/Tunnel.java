b5b13c8dd7d85495cb2419abbed730f023407c31
Dezkiir <Dezkiir@outlook.com>
2020-05-17 12:32:48 +0100

    Commenting

--- java/src/C18339746/Tunnel.java
+++ java/src/C18339746/Tunnel.java
@@ -36,52 +36,57 @@ public class Tunnel extends PApplet {
     }
 
     public void draw() {
-    background(20);
-    translate(width/2, height/2);
-    PVector pv;
-    float fc = (float)frameCount, a;
-    if (dots) beginShape(POINTS); 
+        background(20);
+        translate(width/2, height/2);
+        PVector pv;
+        float fc = (float)frameCount, a;
+        if (dots) beginShape(POINTS); 
 
 
-    for (int i = 0; i < nb; i++) {
-        pv = circles[i];
-        pv.z += zStep;
-        pv.x = (float) ((noise((fc * 2 + pv.z) / 550) - .5) * height * map(pv.z, zMin, zMax, 6, 0));
-        pv.y = (float) ((noise((fc * 2 - 3000 - pv.z) / 550) - .5) * height * map(pv.z, zMin, zMax, 6, 0));
+        for (int i = 0; i < nb; i++) {
+            pv = circles[i];
+            pv.z += zStep;
+            pv.x = (float) ((noise((fc * 2 + pv.z) / 550) - .5) * height * map(pv.z, zMin, zMax, 6, 0));
+            pv.y = (float) ((noise((fc * 2 - 3000 - pv.z) / 550) - .5) * height * map(pv.z, zMin, zMax, 6, 0));
 
-        a = map(pv.z, zMin, zMax, 0, 255);
-        // If not in Black and White mode draw the colours
-        if (!bnw)stroke(colors[i], a);
-        else stroke(map(pv.z, zMin, zMax, 0, 255), a);
-        float r = map(pv.z, zMin, zMax, (float) (rad * .1), rad);
+            a = map(pv.z, zMin, zMax, 0, 255);
+            // If not in Black and White mode draw the colours
+            if (!bnw)stroke(colors[i], a);
+            else stroke(map(pv.z, zMin, zMax, 0, 255), a);
+            float r = map(pv.z, zMin, zMax, (float) (rad * .1), rad);
 
-        // If in Dot Mode
-        if (dots) {
-        float jmax = r;
+            // If in Dot Mode
+            if (dots) {
+                float jmax = r;
 
-        // Loop to draw dots at fixed intervals around the circle
-        for (int j  = 0; j < jmax; j++)
-        {
-            vertex(pv.x + r*cos(j*TWO_PI/jmax + fc/40)/2, pv.y + r*sin(j*TWO_PI/jmax + fc/40)/2, pv.z);
+                // Loop to draw dots at fixed intervals around the circle
+                for (int j  = 0; j < jmax; j++)
+                {
+                    vertex(pv.x + r*cos(j*TWO_PI/jmax + fc/40)/2, pv.y + r*sin(j*TWO_PI/jmax + fc/40)/2, pv.z);
+                }
+            } 
+            else {
+                // Brings forward the circles as they spawn to give illusion of 3D
+                pushMatrix();
+                translate(pv.x, pv.y, pv.z);
+                ellipse(0, 0, r, r);
+                // Removes circles when they reach the centre
+                popMatrix();
+            }
+
+            if (pv.z > zMax) {
+                circles[i].z = zMin;
+            }
         }
-        } else {
-        pushMatrix();
-        translate(pv.x, pv.y, pv.z);
-        ellipse(0, 0, r, r);
-        popMatrix();
-        }
-
-        if (pv.z > zMax) {
-        circles[i].z = zMin;
-        }
+        if (dots) endShape();
     }
-    if (dots) endShape();
-    }
 
+    // Change betweeen Dot Mode on key press (Any Key)
     public void keyPressed(){
         dots = !dots;
     }
 
+    // Change between Black and White mode on Mouse Click
     public void mousePressed(){
         bnw = !bnw;
     }
